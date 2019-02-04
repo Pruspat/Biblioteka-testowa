15351bce7e7e2776ed0a751608712a688466adf6
Patryk <pruspat@gmail.com>
2019-02-04 20:21:28 +0100

    zwracanie tokena w body

--- src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -56,6 +56,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
-        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+//        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.getWriter().append(token);
     }
 }
